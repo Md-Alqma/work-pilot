540578417cf3032231b49c31d8cada7f02be94da
Md-Alqma <mdalqma7@gmail.com>
2025-03-03 07:28:17 +0530

    added shadcn and building auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  return <Button>Test</Button>;
+  return (
+    <div className="flex gap-4">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Detructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="teritary">Teritrary</Button>
+      <Input />
+    </div>
+  );
 }
