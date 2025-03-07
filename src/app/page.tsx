55b4c108ba4cfa3943128e829806223702cda7f1
Md-Alqma <mdalqma7@gmail.com>
2025-03-07 15:07:11 +0530

    logout api with custom appwrite middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,34 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useCurrent } from "@/features/auth/api/user-current";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
+  if (isLoading) {
+    return (
+      <div className="flex min-h-screen justify-center items-center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
   return (
-    <div className="flex gap-4">
-      <Button>Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Detructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="teritary">Teritrary</Button>
-      <Input />
+    <div>
+      Only visible to authorized users.
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
