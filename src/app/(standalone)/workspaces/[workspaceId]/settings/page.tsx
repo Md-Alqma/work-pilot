2e90eabf78261fbf47126c107d6eb070c6466d0d
Md-Alqma <mdalqma7@gmail.com>
2025-03-10 00:25:41 +0530

    edit workspace functionality with image upload and remove

--- src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -1,4 +1,6 @@
 import { getCurrent } from "@/features/auth/actions";
+import { getWorkspace } from "@/features/workspaces/actions";
+import { EditWorkspaceForm } from "@/features/workspaces/components/edit-workspace-form";
 import { redirect } from "next/navigation";
 
 interface WorkspaceIdSettingsPageProps {
@@ -12,7 +14,15 @@ const WorkspaceIdSettingsPage = async ({
 }: WorkspaceIdSettingsPageProps) => {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
-  return <div>Workspace Id settings page: {params.workspaceId}</div>;
+
+  const initialValues = await getWorkspace({ workspaceId: params.workspaceId });
+
+  if (!initialValues) redirect(`/workspaces/${params.workspaceId}`);
+  return (
+    <div className="w-full lg:max-w-xl">
+      <EditWorkspaceForm initialValues={initialValues} />
+    </div>
+  );
 };
 
 export default WorkspaceIdSettingsPage;
--- src/features/workspaces/components/edit-workspace-form.tsx
+++ src/features/workspaces/components/edit-workspace-form.tsx
@@ -16,14 +16,14 @@ import {
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useCreateWorkspace } from "../api/use-create-workspace";
 import { useRef } from "react";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import Image from "next/image";
-import { ImageIcon } from "lucide-react";
+import { ArrowLeftIcon, ImageIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Workspace } from "../types";
+import { useUpdateWorkspace } from "../api/use-update-workspace";
 
 interface EditWorkspaceFormrops {
   onCancel?: () => void;
@@ -36,13 +36,12 @@ export const EditWorkspaceForm = ({
 }: EditWorkspaceFormrops) => {
   const router = useRouter();
 
-  const { mutate, isPending } = useCreateWorkspace();
+  const { mutate, isPending } = useUpdateWorkspace();
   const inputRef = useRef<HTMLInputElement>(null);
 
   const form = useForm<z.infer<typeof updateWorkspaceSchema>>({
     resolver: zodResolver(updateWorkspaceSchema),
     defaultValues: {
-      name: "",
       ...initialValues,
       image: initialValues.imageUrl ?? "",
     },
@@ -51,7 +50,7 @@ export const EditWorkspaceForm = ({
   const onSubmit = (values: z.infer<typeof updateWorkspaceSchema>) => {
     const finalValues = {
       ...values,
-      image: values.image instanceof File ? values.image : undefined,
+      image: values.image instanceof File ? values.image : "",
     };
     mutate(
       { form: finalValues, param: { workspaceId: initialValues.$id } },
@@ -72,7 +71,19 @@ export const EditWorkspaceForm = ({
   };
   return (
     <Card className="w-full h-full border-none shadow-nono">
-      <CardHeader className="flex p-7">
+      <CardHeader className="flex flex-row items-center gap-x-4 p-7 space-y-0">
+        <Button
+          size="sm"
+          variant="secondary"
+          onClick={
+            onCancel
+              ? onCancel
+              : () => router.push(`/workspaces/${initialValues.$id}`)
+          }
+        >
+          <ArrowLeftIcon className="size-4 mr-1" />
+          Back
+        </Button>
         <CardTitle className="text-xl font-bold">
           {initialValues.name}
         </CardTitle>
@@ -136,16 +147,34 @@ export const EditWorkspaceForm = ({
                           onChange={handleImageChange}
                           disabled={isPending}
                         />
-                        <Button
-                          type="button"
-                          disabled={isPending}
-                          variant="teritary"
-                          size="xs"
-                          className="w-fit mt-2"
-                          onClick={() => inputRef.current?.click()}
-                        >
-                          Upload Image
-                        </Button>
+                        {field.value ? (
+                          <Button
+                            type="button"
+                            disabled={isPending}
+                            variant="destructive"
+                            size="xs"
+                            className="w-fit mt-2"
+                            onClick={() => {
+                              field.onChange(null);
+                              if (inputRef.current) {
+                                inputRef.current.value = "";
+                              }
+                            }}
+                          >
+                            Remove Image
+                          </Button>
+                        ) : (
+                          <Button
+                            type="button"
+                            disabled={isPending}
+                            variant="teritary"
+                            size="xs"
+                            className="w-fit mt-2"
+                            onClick={() => inputRef.current?.click()}
+                          >
+                            Upload Image
+                          </Button>
+                        )}
                       </div>
                     </div>
                   </div>
@@ -165,7 +194,7 @@ export const EditWorkspaceForm = ({
                 Cancel
               </Button>
               <Button type="submit" size="lg" disabled={isPending}>
-                Create Workspace
+                Save Changes
               </Button>
             </div>
           </form>
