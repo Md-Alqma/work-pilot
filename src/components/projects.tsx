ef50125f4ac051b8bbda128b0996a9d8f07907ab
Md-Alqma <mdalqma7@gmail.com>
2025-03-11 14:12:16 +0530

    create project functionality

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -8,19 +8,22 @@ import { cn } from "@/lib/utils";
 
 import { useGetProjects } from "@/features/projects/api/use-get-projects";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
+import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 
 export const Projects = () => {
   const projectId = null;
   const workspaceId = useWorkspaceId();
   const { data } = useGetProjects({ workspaceId });
   const pathname = usePathname();
+  const { open } = useCreateProjectModal();
 
   return (
     <div className="flex flex-col gap-y-2">
       <div className="flex items-center justify-between">
         <p className="text-xs uppercase text-neutral-500">Projects</p>
         <RiAddCircleFill
-          onClick={() => {}}
+          onClick={open}
           className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition"
         />
       </div>
@@ -36,6 +39,7 @@ export const Projects = () => {
                 isActive && "bg-white shadow-sm hover:opacity-100 text-primary"
               )}
             >
+              <ProjectAvatar image={project.imageUrl} name={project.name} />
               <span className="truncate">{project.name}</span>
             </div>
           </Link>
