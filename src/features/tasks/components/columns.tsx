e12c528a7d5800179606fcef1520812761058cac
Md-Alqma <mdalqma7@gmail.com>
2025-03-12 12:23:30 +0530

    sorting and filtering tasks working

---
+++ src/features/tasks/components/columns.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+import { Task } from "../types";
+
+export const columns: ColumnDef<Task>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Task Name
+          <ArrowUpDown className="ml-2 size-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      const name = row.original.name;
+      return <p className="line-clamp-1">{name}</p>;
+    },
+  },
+  {
+    accessorKey: 'project',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Project
+          <ArrowUpDown className="ml-2 size-4" />
+        </Button>
+      );
+    },
+  }
+];
